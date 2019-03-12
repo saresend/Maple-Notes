2a7dcba98e8b3c1060ff546f2fa7ad96204332f6
saresend <saresend@usc.edu>
2019-03-12 05:51:49 -0700

    semi styling context menu, so it kinda does stuff now

--- src/reactComponents/ContextMenu.react.js
+++ src/reactComponents/ContextMenu.react.js
@@ -3,9 +3,31 @@ import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 
 const onClick = ({ event, props }) => console.log(event, props);
 
+const menuItemStyle = {
+  "fontFamily": "Aleo",
+  "margin": "0px",
+}
+
+const menuContainerStyle = {
+  "backgroundColor": "#ffffff",
+  "padding": "0px",
+  "borderWidth": "1px",
+  "borderColor": "gray",
+  "borderStyle": "solid",
+  "borderRadius": "10px",
+}
+
+const menuTextStyle = {
+  "padding": "5px",
+  "marginTop": "5px",
+  "marginBottom": "5px",
+  "marginRight": "40px",
+  "fontSize": "17px",
+}
+
 export default class ContextMenuReact extends React.Component {
 
-  handleClick(e) {
+  handleClick(e, data) {
     console.log('Something was clicked');
   }
 
@@ -15,17 +37,13 @@ export default class ContextMenuReact extends React.Component {
         <ContextMenuTrigger id="some_unique_identifier">
           {this.props.children}
         </ContextMenuTrigger>
-        <ContextMenu id="some_unique_identifier" style={{ "backgroundColor": "#ffffff" }}>
-          <MenuItem data={{ foo: 'bar' }} onClick={this.handleClick}>
-            ContextMenu Item 1
-        </MenuItem>
-          <MenuItem data={{ foo: 'bar' }} onClick={this.handleClick}>
-            ContextMenu Item 2
-        </MenuItem>
-          <MenuItem divider />
-          <MenuItem data={{ foo: 'bar' }} onClick={this.handleClick}>
-            ContextMenu Item 3
-        </MenuItem>
+        <ContextMenu id="some_unique_identifier" style={menuContainerStyle}>
+          <MenuItem style={menuItemStyle} data={{ foo: 'bar' }} onClick={this.handleClick}>
+            <p className="contextMenuHover" style={menuTextStyle}>Rename</p>
+          </MenuItem>
+          <MenuItem style={menuItemStyle} data={{ foo: 'bar' }} onClick={this.handleClick}>
+            <p className="contextMenuHover" style={menuTextStyle}>Delete</p>
+          </MenuItem>
         </ContextMenu>
       </div>
 
